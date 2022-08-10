381257052fb1ac3505c575f0b3efbd3a912cb127
oki rakhim <oki.rakhim@dkatalis.com>
2022-08-10 20:50:01 +0700

    remove italian

--- components/language-bar.js
+++ components/language-bar.js
@@ -4,15 +4,10 @@ import { useRouter } from "next/router";
 export default function LanguageBar() {
   const currentPath = useRouter().asPath;
   return (
-    <section className="flex-col md:flex-row flex items-center md:justify-end mr-0 mt-0 mb-0 md:mb-0">
-      <Link href={currentPath} locale="it">
-        <a className="underline hover:text-success duration-200 transition-colors p-1">
-          Italian
-        </a>
-      </Link>
+    <section className="flex-col md:flex-row flex items-center md:justify-end mr-0 mt-0 mb-0 md:mb-0">      
       <Link href={currentPath} locale="id">
         <a className="underline hover:text-success duration-200 transition-colors p-1">
-          Italian
+          Indonesia
         </a>
       </Link>
       <Link href={currentPath} locale="en">
