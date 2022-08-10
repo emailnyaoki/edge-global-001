06fa794a8a24dbcb1ac78405f66b3c161884f786
oki rakhim <oki.rakhim@dkatalis.com>
2022-08-10 20:58:32 +0700

    chores: removing parts

--- components/footer.js
+++ components/footer.js
@@ -12,18 +12,8 @@ export default function Footer() {
             {i18n.footer.static[locale]}
           </h3>
           <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
-            <a
-              href="https://www.datocms.com/docs/next-js"
-              className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
-            >
-              {i18n.footer.read[locale]}
-            </a>
-            <a
-              href="https://github.com/datocms/nextjs-demo"
-              className="mx-3 font-bold hover:underline"
-            >
-              {i18n.footer.github[locale]}
-            </a>
+            
+            
           </div>
         </div>
       </Container>
